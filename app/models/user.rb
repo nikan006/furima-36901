f2cfd37d4b46887f6e4a8fca9612e528dc3f3012
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-01 11:47:21 +0900

    リファクタリングの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_PASSWORD_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_PASSWORD_SURNAME = /\A[ァ-ヶー－]+\z/
+
   validates :nickname, presence: true
   validates :password, format: {with:/(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-  validates :family_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
+  validates :family_name, presence: true, format: {with:VALID_PASSWORD_REGEX}
+  validates :first_name, presence: true, format: {with:VALID_PASSWORD_REGEX}
+  validates :family_name_kana, presence: true, format: {with:VALID_PASSWORD_SURNAME}
+  validates :first_name_kana, presence: true, format: {with:VALID_PASSWORD_SURNAME}
   validates :birth_date, presence: true
 end
