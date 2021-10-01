0861ca1c6daf9a4d626ecc46a5251d83ebbafa5d
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-01 12:18:58 +0900

    テストコードの追加実装修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -58,6 +58,11 @@ RSpec.describe User, type: :model do
       another_user.valid?
       expect(another_user.errors.full_messages).to include("Email has already been taken")
     end
+    it "emailに@がないと登録できない" do
+      @user.email = "testgamil.com"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email is invalid")
+    end
     it "passwordが5文字以下では登録できない" do
       @user.password = '00000'
       @user.password_confirmation = '00000'
