65fd1bc28250d3b0d3e45dc4b3cb3c8754fee349
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-01 10:17:55 +0900

    バリデーションの修正,テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :password, format: {with:/(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+  validates :family_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true, format: {with:/\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
+  validates :first_name_kana, presence: true, format: {with:/\A[ァ-ヶー－]+\z/}
   validates :birth_date, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname                 {Faker::Name.initials(number: 2)}
+    nickname                 {"test"}
     email                    {Faker::Internet.free_email}
     password                 {Faker::Internet.password(min_length: 6)}
     password_confirmation    {password}
+    family_name              {"檸檬"}
+    family_name_kana         {"レモン"}
+    first_name               {"蜜柑"}
+    first_name_kana          {"ミカン"}
+    birth_date               {"2000-01-01"}
   end
 end
