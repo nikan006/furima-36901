166f6c08d66c2301551f588ee1829d19c15d548d
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-02 10:46:31 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,21 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_day
   has_one_attached :image
 
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :fee_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :image
+    validates :price
+  end
+  
+  validates :price, numericality: {with:/\A[0-9]+\z/, message: "Half-width number"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "Out of setting range"}
+
+  with_options presence: true, numericality: { other_than: 1, message: "can't be blank"} do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 end
