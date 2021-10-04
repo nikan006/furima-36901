a9ccf5c5b9f087507e0ea29bb9a175f293c2a114
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-04 11:27:33 +0900

    テストコードの追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,6 +67,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price half-width number")
       end
+      it "価格が9_999_999を超えると出品できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price out of setting range")
+      end
+      it "userが紐づいていなければ出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end
