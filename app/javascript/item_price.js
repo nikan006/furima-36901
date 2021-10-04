049788d56521d4b05cd25226c472670bab27eb7f
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-04 12:31:14 +0900

    修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -10,6 +10,5 @@ window.addEventListener("load",() => {
     const profitNumber = document.getElementById("profit")
     const value_result = inputValue * 0.1
     profitNumber.innerHTML = (Math.floor(inputValue - value_result))
-    console.log(profitNumber);
   })
 });
