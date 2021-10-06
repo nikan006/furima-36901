73f4e6ca9873308de91ea14a85ea5c34b48a6eed
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-06 11:12:02 +0900

    URL直接入力の制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
+  before_action :move_to_index, except: [:index, :show]
 
 
   def index
@@ -25,6 +26,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -40,6 +42,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :text, :price, :category_id, :condition_id, :fee_id, :prefecture_id, :shipping_day_id,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
 end
