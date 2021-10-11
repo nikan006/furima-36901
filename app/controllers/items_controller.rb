000d50623eb4820e813eab76895d61f3bd20ecb8
岸拓成 <88487269+nikan006@users.noreply.github.com>
2021-10-11 15:49:32 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def index
@@ -26,6 +26,9 @@ class ItemsController < ApplicationController
   def edit
     @item = Item.find(params[:id])
     redirect_to root_path unless current_user.id == @item.user_id
+    if current_user == @item.user || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def update
